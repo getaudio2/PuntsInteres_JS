00a373742e1fc81ccb2ebadf39fb072074167faf
getaudio2 <paugermaisar@hotmail.es>
2025-04-06 22:11:14 +0200

    Fixed map geolocation problem with promise

--- script/Mapa.js
+++ script/Mapa.js
@@ -1,18 +1,24 @@
 class Mapa {
     #map;
-    #currentLat;
-    #currentLng;
+    #currentLat = 1;
+    #currentLng = 1;
     #markers = [];
 
     constructor(){
-        this.#getPosicioActual();
+        this.#init();
+    }
+
+    async #init() {
+        await this.#getPosicioActual();
+
         const mapCenter = [this.#currentLat, this.#currentLng];
         const zoomLevel = 13;
 
-        this.#map = L.map('map').setView(mapCenter, zoomLevel);      
+        this.#map = L.map('map').setView(mapCenter, zoomLevel);
+
         let tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '© OpenStreetMap contributors'
-        })
+        });
         tileLayer.addTo(this.#map);
 
         L.marker(mapCenter).addTo(this.#map).bindPopup("Estàs aquí").openPopup();
@@ -36,23 +42,24 @@ class Mapa {
         this.#markers = [];
     }
 
-    #getPosicioActual(){
-        let lat = 1;
-        let lng = 1;
+    async #getPosicioActual(){
+        return new Promise((resolve) => {
+            if (!navigator.geolocation) {
+                console.error("La geolocalització no està disponible.");
+                return resolve();
+            }
 
-        // Verifica si la geolocalización está disponible en el navegador
-        if (!this.#currentLat && !this.#currentLng) {
-            navigator.geolocation.getCurrentPosition(function (position) {
-                lat = position.coords.latitude;
-                lng = position.coords.longitude;
-            }, function (error) {
-                console.error("Error en la geolocalización:", error);
-            });
-        } else {
-            console.error("La geolocalización no está disponible en este navegador.");
-        }
-
-        this.#currentLat = lat;
-        this.#currentLng = lng;
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    this.#currentLat = position.coords.latitude;
+                    this.#currentLng = position.coords.longitude;
+                    resolve();
+                },
+                (error) => {
+                    console.error("Error en la geolocalització:", error);
+                    resolve();
+                }
+            );
+        });
     }
 }
